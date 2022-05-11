3a324e6545736552bca8835d6ca3a6a8be97a7ed
robalford <robertsalford@gmail.com>
2022-05-11 14:28:18 -0700

    add static folder with manifest and icons. add pwa markup to home.html

--- django_pwa/settings.py
+++ django_pwa/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    Path(BASE_DIR, "django_pwa", "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
