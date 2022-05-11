a0d2ce7abe5591558bb2dd54d117891a82f8520b
robalford <robertsalford@gmail.com>
2022-05-11 13:56:13 -0700

    add core app with home and offline pages

--- django_pwa/urls.py
+++ django_pwa/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home',),
+    path('offline/', TemplateView.as_view(template_name='offline.html'), name='offline',),
     path('admin/', admin.site.urls),
 ]
